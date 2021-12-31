129eb89ca46f59f505813e083b0a2b70cb4ec07d
SwethuMurali <89376639+SwethuMurali@users.noreply.github.com>
2021-12-31 20:45:00 +0530

    Create aodv.tcl

---
+++ aodv.tcl
@@ -0,0 +1,287 @@
+#===================================
+#     Simulation parameters setup
+#===================================
+set val(chan)   Channel/WirelessChannel    ;# channel type
+set val(prop)   Propagation/TwoRayGround   ;# radio-propagation model
+set val(netif)  Phy/WirelessPhy            ;# network interface type
+set val(mac)    Mac/802_11                 ;# MAC type
+set val(ifq)    Queue/DropTail/PriQueue    ;# interface queue type
+set val(ll)     LL                         ;# link layer type
+set val(ant)    Antenna/OmniAntenna        ;# antenna model
+set val(ifqlen) 10                         ;# max packet in ifq
+set val(nn)     32                         ;# number of mobilenodes
+set val(rp)     AODV                       ;# routing protocol
+set val(x)      1290                      ;# X dimension of topography
+set val(y)      531                      ;# Y dimension of topography
+set val(stop)   10.0                         ;# time of simulation end
+
+#===================================
+#        Initialization        
+#===================================
+#Create a ns simulator
+set ns [new Simulator]
+
+#Setup topography object
+set topo       [new Topography]
+$topo load_flatgrid $val(x) $val(y)
+create-god $val(nn)
+
+#Open the NS trace file
+set tracefile [open AODV.tr w]
+$ns trace-all $tracefile
+
+#Open the NAM trace file
+set namfile [open AODV.nam w]
+$ns namtrace-all $namfile
+$ns namtrace-all-wireless $namfile $val(x) $val(y)
+set chan [new $val(chan)];#Create wireless channel
+
+#===================================
+#     Mobile node parameter setup
+#===================================
+$ns node-config -adhocRouting  $val(rp) \
+                -llType        $val(ll) \
+                -macType       $val(mac) \
+                -ifqType       $val(ifq) \
+                -ifqLen        $val(ifqlen) \
+                -antType       $val(ant) \
+                -propType      $val(prop) \
+                -phyType       $val(netif) \
+                -channel       $chan \
+                -topoInstance  $topo \
+                -energyModel   "EnergyModel" \
+                -initialEnergy 50.0 \
+                -txPower 0.9 \
+                -rxPower 0.7 \
+                -idlePower 0.6 \
+                -sleepPower 0.1 \
+                -agentTrace    ON \
+                -routerTrace   ON \
+                -macTrace      ON \
+                -movementTrace ON
+
+#===================================
+#        Nodes Definition        
+#===================================
+#Create 32 nodes
+set n0 [$ns node]
+$n0 set X_ 49
+$n0 set Y_ 412
+$n0 set Z_ 0.0
+$ns initial_node_pos $n0 20
+set n1 [$ns node]
+$n1 set X_ 49
+$n1 set Y_ 412
+$n1 set Z_ 0.0
+$ns initial_node_pos $n1 20
+set n2 [$ns node]
+$n2 set X_ 49
+$n2 set Y_ 412
+$n2 set Z_ 0.0
+$ns initial_node_pos $n2 20
+set n3 [$ns node]
+$n3 set X_ 54
+$n3 set Y_ 333
+$n3 set Z_ 0.0
+$ns initial_node_pos $n3 20
+set n4 [$ns node]
+$n4 set X_ 159
+$n4 set Y_ 339
+$n4 set Z_ 0.0
+$ns initial_node_pos $n4 20
+set n5 [$ns node]
+$n5 set X_ 167
+$n5 set Y_ 381
+$n5 set Z_ 0.0
+$ns initial_node_pos $n5 20
+set n6 [$ns node]
+$n6 set X_ 102
+$n6 set Y_ 173
+$n6 set Z_ 0.0
+$ns initial_node_pos $n6 20
+set n7 [$ns node]
+$n7 set X_ 47
+$n7 set Y_ 206
+$n7 set Z_ 0.0
+$ns initial_node_pos $n7 20
+set n8 [$ns node]
+$n8 set X_ 161
+$n8 set Y_ 234
+$n8 set Z_ 0.0
+$ns initial_node_pos $n8 20
+set n9 [$ns node]
+$n9 set X_ 262
+$n9 set Y_ 275
+$n9 set Z_ 0.0
+$ns initial_node_pos $n9 20
+set n10 [$ns node]
+$n10 set X_ 305
+$n10 set Y_ 384
+$n10 set Z_ 0.0
+$ns initial_node_pos $n10 20
+set n11 [$ns node]
+$n11 set X_ 240
+$n11 set Y_ 424
+$n11 set Z_ 0.0
+$ns initial_node_pos $n11 20
+set n12 [$ns node]
+$n12 set X_ 238
+$n12 set Y_ 59
+$n12 set Z_ 0.0
+$ns initial_node_pos $n12 20
+set n13 [$ns node]
+$n13 set X_ 363
+$n13 set Y_ 116
+$n13 set Z_ 0.0
+$ns initial_node_pos $n13 20
+set n14 [$ns node]
+$n14 set X_ 448
+$n14 set Y_ 226
+$n14 set Z_ 0.0
+$ns initial_node_pos $n14 20
+set n15 [$ns node]
+$n15 set X_ 405
+$n15 set Y_ 328
+$n15 set Z_ 0.0
+$ns initial_node_pos $n15 20
+set n16 [$ns node]
+$n16 set X_ 458
+$n16 set Y_ 427
+$n16 set Z_ 0.0
+$ns initial_node_pos $n16 20
+set n17 [$ns node]
+$n17 set X_ 543
+$n17 set Y_ 295
+$n17 set Z_ 0.0
+$ns initial_node_pos $n17 20
+set n18 [$ns node]
+$n18 set X_ 711
+$n18 set Y_ 282
+$n18 set Z_ 0.0
+$ns initial_node_pos $n18 20
+set n19 [$ns node]
+$n19 set X_ 678
+$n19 set Y_ 400
+$n19 set Z_ 0.0
+$ns initial_node_pos $n19 20
+set n20 [$ns node]
+$n20 set X_ 595
+$n20 set Y_ 169
+$n20 set Z_ 0.0
+$ns initial_node_pos $n20 20
+set n21 [$ns node]
+$n21 set X_ 533
+$n21 set Y_ 66
+$n21 set Z_ 0.0
+$ns initial_node_pos $n21 20
+set n22 [$ns node]
+$n22 set X_ 101
+$n22 set Y_ 69
+$n22 set Z_ 0.0
+$ns initial_node_pos $n22 20
+set n23 [$ns node]
+$n23 set X_ 794
+$n23 set Y_ 100
+$n23 set Z_ 0.0
+$ns initial_node_pos $n23 20
+set n24 [$ns node]
+$n24 set X_ 860
+$n24 set Y_ 233
+$n24 set Z_ 0.0
+$ns initial_node_pos $n24 20
+set n25 [$ns node]
+$n25 set X_ 835
+$n25 set Y_ 352
+$n25 set Z_ 0.0
+$ns initial_node_pos $n25 20
+set n26 [$ns node]
+$n26 set X_ 887
+$n26 set Y_ 431
+$n26 set Z_ 0.0
+$ns initial_node_pos $n26 20
+set n27 [$ns node]
+$n27 set X_ 987
+$n27 set Y_ 335
+$n27 set Z_ 0.0
+$ns initial_node_pos $n27 20
+set n28 [$ns node]
+$n28 set X_ 971
+$n28 set Y_ 207
+$n28 set Z_ 0.0
+$ns initial_node_pos $n28 20
+set n29 [$ns node]
+$n29 set X_ 1083
+$n29 set Y_ 82
+$n29 set Z_ 0.0
+$ns initial_node_pos $n29 20
+set n30 [$ns node]
+$n30 set X_ 1135
+$n30 set Y_ 390
+$n30 set Z_ 0.0
+$ns initial_node_pos $n30 20
+set n31 [$ns node]
+$n31 set X_ 1190
+$n31 set Y_ 287
+$n31 set Z_ 0.0
+$ns initial_node_pos $n31 20
+
+#===================================
+#        Generate movement          
+#===================================
+$ns at 2.0 " $n14 setdest 500 60 20 " 
+
+#===================================
+#        Agents Definition        
+#===================================
+#Setup a TCP connection
+set tcp0 [new Agent/TCP]
+$ns attach-agent $n4 $tcp0
+set sink2 [new Agent/TCPSink]
+$ns attach-agent $n27 $sink2
+$ns connect $tcp0 $sink2
+$tcp0 set packetSize_ 1500
+
+#Setup a TCP connection
+set tcp1 [new Agent/TCP]
+$ns attach-agent $n23 $tcp1
+set sink3 [new Agent/TCPSink]
+$ns attach-agent $n12 $sink3
+$ns connect $tcp1 $sink3
+$tcp1 set packetSize_ 1500
+
+
+#===================================
+#        Applications Definition        
+#===================================
+#Setup a FTP Application over TCP connection
+set ftp0 [new Application/FTP]
+$ftp0 attach-agent $tcp0
+$ns at 2.0 "$ftp0 start"
+$ns at 9.0 "$ftp0 stop"
+
+#Setup a FTP Application over TCP connection
+set ftp1 [new Application/FTP]
+$ftp1 attach-agent $tcp1
+$ns at 4.0 "$ftp1 start"
+$ns at 9.0 "$ftp1 stop"
+
+
+#===================================
+#        Termination        
+#===================================
+#Define a 'finish' procedure
+proc finish {} {
+    global ns tracefile namfile
+    $ns flush-trace
+    close $tracefile
+    close $namfile
+    exec nam AODV.nam &
+    exit 0
+}
+for {set i 0} {$i < $val(nn) } { incr i } {
+    $ns at $val(stop) "\$n$i reset"
+}
+$ns at $val(stop) "$ns nam-end-wireless $val(stop)"
+$ns at $val(stop) "finish"
+$ns at $val(stop) "puts \"done\" ; $ns halt"
+$ns run
